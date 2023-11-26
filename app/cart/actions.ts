40eb9d3bf51037f3a5544c73dc6e55cca09b15e8
Ali Şahin <57685811+alisahindev@users.noreply.github.com>
2023-11-26 15:02:21 +0300

    feat: order done

---
+++ app/cart/actions.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { getClient } from "@/graphql/lib/client";
+import { CREATE_ORDER } from "@/graphql/queries/order/mutation";
+import { readIdFromCookies } from "../actions";
+
+export const createOrderAction = async (
+  tenantGrouped: any,
+  totalPrice: any
+) => {
+  let userId = "";
+
+  userId = await readIdFromCookies("user_id");
+
+  if (!userId) return null;
+
+  const tenant_orders = Object.keys(tenantGrouped).map((key) => {
+    const tenantItems = tenantGrouped[key];
+    return {
+      tenant_id: key,
+      order_items: {
+        data: tenantItems.map((item) => ({
+          product_id: item.id,
+          quantity: item.quantity,
+          user_id: userId,
+          order_item_special_texts: {
+            data: [
+              { content: "enes sahin yazuılsın" },
+              { content: "enes sahin yazuılsın" },
+            ],
+          },
+        })),
+      },
+    };
+  });
+
+  const variables = {
+    user_id: userId,
+    total_amount: totalPrice,
+    tenant_orders,
+    order_addresses: [
+      {
+        address: "asalet sokak",
+        address_title: "ev",
+        city_id: 1,
+        quarter_id: 1,
+        district_id: 1,
+        receiver_phone: "045456347383",
+        receiver_surname: "ömer",
+        receiver_firstname: "şahin",
+      },
+    ],
+  };
+
+  const response = await getClient().mutate({
+    mutation: CREATE_ORDER,
+    variables,
+  });
+
+  return response;
+};
--- graphql/queries/order/mutation.ts
+++ graphql/queries/order/mutation.ts
@@ -1,9 +1,19 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_ORDER = gql`
-  mutation MyMutation($user_id: uuid!, $total_amount: numeric!, $tenant_orders: [order_tenant_insert_input!]!) {
+  mutation MyMutation(
+    $user_id: uuid!
+    $total_amount: numeric!
+    $tenant_orders: [order_tenant_insert_input!]!
+    $order_addresses: [order_address_insert_input!]!
+  ) {
     insert_order_one(
-      object: { user_id: $user_id, total_amount: $total_amount, tenant_orders: { data: $tenant_orders } }
+      object: {
+        user_id: $user_id
+        total_amount: $total_amount
+        tenant_orders: { data: $tenant_orders }
+        order_addresses: { data: $order_addresses }
+      }
     ) {
       id
     }
