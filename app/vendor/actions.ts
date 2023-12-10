c66e7a1d87baddb982c8fd6c3608b1e67044dca7
bulutme <bltbey06@gmail.com>
2023-12-11 00:03:59 +0300

    feat: develop vendor page

---
+++ app/vendor/actions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { getClient } from "@/graphql/lib/client";
+import { GET_VENDOR_PRODUCTS_WITH_PAGINATION } from "@/graphql/queries/vendors/getVendorProducstWithPagination";
+
+export const getPaginatedVendorProducts = async <T>({
+  offset,
+  tenant_id,
+}: {
+  offset: number;
+  tenant_id: string;
+}) => {
+  const { data } = await getClient().query({
+    query: GET_VENDOR_PRODUCTS_WITH_PAGINATION,
+    variables: {
+      offset,
+      tenant_id,
+    },
+  });
+
+  return {
+    products: data.product,
+    totalCount: data.product_aggregate.aggregate.count,
+  } as T;
+};
