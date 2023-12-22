97ea1c02743bb1ea0df8606afb639aa836e18519
Enes Sahin <iamfreeadmiral@gmail.com>
2023-12-22 22:10:15 +0300

    chat page animation

--- app/account/messages/components/Message/MessageList.tsx
+++ app/account/messages/components/Message/MessageList.tsx
@@ -1,3 +1,4 @@
+"use client";
 import MessageItem from "./MessageItem";
 
 const data = {
@@ -7,11 +8,15 @@ const data = {
   picture: "https://tailwindcss-chat.vercel.app/resources/profile-image.png",
 };
 
-const MessageList = (props) => {
+const MessageList = () => {
   return (
     <div className="messages flex-1 overflow-auto">
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-        <MessageItem key={item} {...data} type={item % 2 === 0 ? "sent" : "received"} />
+        <MessageItem
+          key={item}
+          {...data}
+          type={item % 2 === 0 ? "sent" : "received"}
+        />
       ))}
     </div>
   );
