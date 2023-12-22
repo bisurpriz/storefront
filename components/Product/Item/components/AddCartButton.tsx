de8d4b34d323ff4eec1e53ea03e9b16dddc23a98
Enes Sahin <iamfreeadmiral@gmail.com>
2023-12-22 22:50:15 +0300

    redis initiliazed

--- components/Product/Item/components/AddCartButton.tsx
+++ components/Product/Item/components/AddCartButton.tsx
@@ -3,19 +3,41 @@ import Button from "@/components/Button";
 import useCart from "@/store/cart";
 import { useCallback } from "react";
 import { ProductItemProps } from "..";
+import { setRedisProduct } from "@/app/cart/actions";
 
-const AddCartButton = ({ id, loading, product_customizable_areas, tenant_id }: ProductItemProps) => {
+const AddCartButton = ({
+  id,
+  loading,
+  product_customizable_areas,
+  tenant_id,
+}: ProductItemProps) => {
   const { addToCart, cartItems } = useCart.getState();
 
-  const handleAddToCart = useCallback(() => {
+  const handleAddToCart = useCallback(async () => {
     const prev = cartItems.find((item) => item.id === id);
     const hasSpecialInstructions = product_customizable_areas?.length > 0;
     addToCart({
       id,
       quantity: prev ? prev.quantity + 1 : 1,
-      specialInstructions: prev?.specialInstructions ? prev.specialInstructions : hasSpecialInstructions ? [] : null,
+      specialInstructions: prev?.specialInstructions
+        ? prev.specialInstructions
+        : hasSpecialInstructions
+        ? []
+        : null,
       tenant_id,
     });
+
+    const rest = await setRedisProduct({
+      id,
+      quantity: prev ? prev.quantity + 1 : 1,
+      specialInstructions: prev?.specialInstructions
+        ? prev.specialInstructions
+        : hasSpecialInstructions
+        ? []
+        : null,
+      tenant_id,
+    });
+    console.log(rest);
   }, [id, addToCart, cartItems, product_customizable_areas]);
 
   return (
