f6de812ba1e34a520a71c45b530c710b5fa99b3a
ali.sahin <ali.sahin@trendyol.com>
2024-06-16 00:18:32 +0300

    feat: Optimize page load performance in OrderCompletePage

--- app/cart/loading.tsx
+++ app/cart/loading.tsx
@@ -1,19 +1,6 @@
-"use client";
+import React from "react";
 
-import CartSteps from "./components/Cart/CartSteps";
-import EmptyCart from "./components/Cart/EmptyCart";
-import { useCart } from "@/contexts/CartContext";
-import { CartStepProvider } from "@/contexts/CartContext/CartStepProvider";
-import clsx from "clsx";
-import { usePathname } from "next/navigation";
-import { CartStepPaths } from "./constants";
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
-
-const DynamicSummary = dynamic(
-  () => import("./components/Cart/Summary/CartSummary")
-);
-const renderSuspense = () => {
+const CartLoading = () => {
   return (
     <div className="w-full p-4 bg-slate-600 animate-pulse rounded-lg">
       <div className="flex items-center justify-between mb-4">
@@ -46,40 +33,8 @@ const renderSuspense = () => {
         </div>
         <div className="w-8 h-8 bg-slate-700 rounded-lg"></div>
       </div>
-      <div className="w-full h-8 bg-slate-700 rounded-lg mt-4" />
     </div>
   );
 };
 
-const CartTemplate = ({ children }: { children: React.ReactNode }) => {
-  const {
-    cartState: { count },
-  } = useCart();
-  const pathname = usePathname();
-  if (!count && pathname !== CartStepPaths.COMPLETE) {
-    return <EmptyCart />;
-  }
-
-  return (
-    <CartStepProvider>
-      <section id="cart" className="flex flex-col gap-4 max-md:mb-[60px]">
-        <CartSteps />
-        <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-8 mt-4">
-          <div
-            className={clsx(
-              `col-span-1 md:col-span-2 flex flex-col gap-3`,
-              pathname === CartStepPaths.COMPLETE && "col-span-3 md:col-span-3"
-            )}
-          >
-            {children}
-          </div>
-          <Suspense fallback={renderSuspense()}>
-            <DynamicSummary />
-          </Suspense>
-        </div>
-      </section>
-    </CartStepProvider>
-  );
-};
-
-export default CartTemplate;
+export default CartLoading;
