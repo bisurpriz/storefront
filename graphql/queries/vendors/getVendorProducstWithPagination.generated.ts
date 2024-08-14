7064d5b0faea7952ca18fc99d211b3005302f14f
Enes Sahin <iamfreeadmiral@gmail.com>
2024-08-14 15:36:54 +0300

    feat: Update GraphQL queries for vendor products and vendor by ID
    The GraphQL queries for retrieving vendor products with pagination and vendor by ID have been updated to include additional filtering and nested fields. This change improves the accuracy and efficiency of fetching vendor-related data.

--- graphql/queries/vendors/getVendorProducstWithPagination.generated.ts
+++ graphql/queries/vendors/getVendorProducstWithPagination.generated.ts
@@ -6,7 +6,7 @@ export type GetVendorProductsWithPaginationQueryVariables = Types.Exact<{
   limit?: Types.InputMaybe<Types.Scalars['Int']['input']>;
   offset?: Types.InputMaybe<Types.Scalars['Int']['input']>;
   is_active?: Types.InputMaybe<Types.Scalars['Boolean']['input']>;
-  tenant_id: Types.Scalars['uuid']['input'];
+  tenant_id?: Types.Scalars['uuid']['input'];
 }>;
 
 
@@ -14,9 +14,9 @@ export type GetVendorProductsWithPaginationQuery = { product_aggregate: { aggreg
 
 
 export const GetVendorProductsWithPaginationDocument = gql`
-    query getVendorProductsWithPagination($limit: Int = 10, $offset: Int = 0, $is_active: Boolean = true, $tenant_id: uuid!) {
+    query getVendorProductsWithPagination($limit: Int = 10, $offset: Int = 0, $is_active: Boolean = true, $tenant_id: uuid! = "39d3f5ba-3029-48b2-ae57-d22b249ec6bd") {
   product_aggregate(
-    where: {is_active: {_eq: $is_active}, tenant_id: {_eq: $tenant_id}}
+    where: {is_active: {_eq: $is_active}, tenant: {tenants: {id: {_eq: $tenant_id}}}}
   ) {
     aggregate {
       count
@@ -25,7 +25,7 @@ export const GetVendorProductsWithPaginationDocument = gql`
   product(
     limit: $limit
     offset: $offset
-    where: {is_active: {_eq: $is_active}, tenant_id: {_eq: $tenant_id}}
+    where: {is_active: {_eq: $is_active}, tenant: {tenants: {id: {_eq: $tenant_id}}}}
   ) {
     id
     description
