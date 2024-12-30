3c679d0908d2d518c924d4e6e28f426bbe0d434b
ali.sahin <ali.sahin@trendyol.com>
2024-12-30 18:18:11 +0300

    Refactor Account Navigation and Layout for Improved Responsiveness and User Experience
    - Updated AccountLayout component to remove unnecessary gap styling for better layout consistency.
    - Enhanced AccountNavigation component with improved active link handling and smooth scrolling for mobile devices.
    - Introduced responsive design adjustments to navigation items, ensuring better usability across different screen sizes.
    
    These changes aim to enhance the overall user experience and maintainability of the account management features.

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -3,7 +3,7 @@ import AccountNavigation from "./components/Navigation";
 
 const AccountLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative flex gap-8 max-md:flex-col max-md:gap-4">
+    <div className="relative flex max-md:flex-col max-md:gap-4">
       <AccountNavigation />
       <div
         className={cn("flex min-h-[50dvh] flex-1 flex-col max-sm:h-[70dvh]")}
