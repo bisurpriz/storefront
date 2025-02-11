14f606c9d75f984dae6f94ac02513c9427eae21e
ali.sahin <alisahindev@gmail.com>
2025-02-11 16:23:00 +0300

    feat: Add Valentine's Day banner with snowfall animation

--- components/Layout/DesignLayout.tsx
+++ components/Layout/DesignLayout.tsx
@@ -1,4 +1,5 @@
 import { GetAllCategoriesQuery } from "@/graphql/queries/categories/getCategories.generated";
+import ValentinesBanner from "../Banner/ValentinesBanner";
 import MobileBottomNav from "../MobileBottomNav";
 import { Toaster } from "../ui/toaster";
 import Footer from "./Footer";
@@ -12,15 +13,16 @@ export default function DesignLayout({
   categories: GetAllCategoriesQuery["category"];
 }) {
   return (
-    <div className="flex min-h-dvh flex-col">
+    <div className="flex flex-col min-h-dvh">
       <Toaster />
+      <ValentinesBanner />
       <Header category={categories} />
 
-      <div className="relative flex flex-1 flex-col overflow-hidden">
+      <div className="relative flex flex-col flex-1 overflow-hidden">
         {/* Scrollable content area */}
         <div className="absolute inset-0 flex flex-col overflow-hidden overflow-y-auto">
           <main className="relative flex-1">
-            <div className="container mx-auto max-w-7xl px-2 py-6 pt-2 max-md:mb-20 max-sm:py-2 sm:px-4">
+            <div className="container px-2 py-6 pt-2 mx-auto max-w-7xl max-md:mb-20 max-sm:py-2 sm:px-4">
               {children}
             </div>
           </main>
